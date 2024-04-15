98a318a891f2d564a5ebcf9ed857f16e06c0f64a
Alexander Jermann <apj2125@columbia.edu>
2024-04-14 22:47:09 -0400

    fixed download code

--- s5/data/miami/get_common_voice.py
+++ s5/data/miami/get_common_voice.py
@@ -1,8 +1,8 @@
 from datasets import load_dataset
 
 def main():
-    cv_16 = load_dataset("mozilla-foundation/common_voice_16_1", "en")
-    datasets.save_to_disk('cv_16-1_en')
+    cv_16 = load_dataset("mozilla-foundation/common_voice_16_1", "en" split="dev")
+    cv_16.save_to_disk('cv_16-1_en')
 
 if __name__ == "__main__":
     main()
