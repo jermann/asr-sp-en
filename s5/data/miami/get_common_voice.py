2ba18ab78617c259bf3339e66d91d73acfe31f83
Alexander Jermann <apj2125@columbia.edu>
2024-04-14 23:33:43 -0400

    fixed download code

--- s5/data/miami/get_common_voice.py
+++ s5/data/miami/get_common_voice.py
@@ -4,8 +4,11 @@ import os
 import io
 
 def main():
+    # Ensure ffmpeg is available for pydub to use (check your system path!)
+    AudioSegment.converter = "/usr/bin/ffmpeg"  # Adjust this path to the ffmpeg installation path on your system
+
     # Load the dataset split
-    dataset = load_dataset("mozilla-foundation/common_voice_16_1", "en", split="dev")
+    dataset = load_dataset("mozilla-foundation/common_voice_16_1", "en", split="validation")
 
     # Directory where the audio files will be saved
     output_dir = 'cv_16-1_en_audio'
@@ -17,7 +20,7 @@ def main():
         # Path to save the audio file
         file_path = os.path.join(output_dir, f"{record['client_id']}_{i}.mp3")
 
-        # Assuming the audio is already in mp3 format and is stored as a binary array in record['audio']['array']
+        # Assuming the audio is in mp3 format and stored as a binary blob in record['audio']['array']
         audio = AudioSegment.from_file(io.BytesIO(record['audio']['array']), format="mp3")
         audio.export(file_path, format="mp3")
 
