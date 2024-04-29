3c74ce14e7d8b5d0b83f825b68c269d1ec570506
Alexander Jermann <apj2125@columbia.edu>
2024-04-29 17:56:52 -0400

    Expanded Run_v2.sh

--- s5/local/make_train_test.sh
+++ s5/local/make_train_test.sh
@@ -13,6 +13,11 @@ utils/subset_data_dir.sh --utt-list shuffled_test data/miami/bangortalk data/mia
 # removes temp lists
 rm shuffled_list shuffled_train shuffled_test
 
+# Remove Segments File
+
+# Adding Absolute Path to Wav.scp
+# awk 'BEGIN { FS="\t" } { $2 = "/home/apj2125/kaldi-trunk/egs/asr-sp-en/s5/data/miami/" $2 } 1' wav.scp > wav_absolute.scp
+
 # # splits non-training data into validation and testing
 # utils/subset_data_dir.sh --first data/${trans_type}_deveval 20 data/${dev_set}
 # utils/subset_data_dir.sh --last data/${trans_type}_deveval 20 data/${eval_set}
---
+++ s5/run_basic_works.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+#
+# Based mostly on the Switchboard recipe. The training database is TED-LIUM,
+# it consists of TED talks with cleaned automatic transcripts:
+#
+# https://lium.univ-lemans.fr/ted-lium3/
+# http://www.openslr.org/resources (Mirror).
+#
+# The data is distributed under 'Creative Commons BY-NC-ND 3.0' license,
+# which allow free non-commercial use, while only a citation is required.
+#
+# Copyright  2014  Nickolay V. Shmyrev
+#            2014  Brno University of Technology (Author: Karel Vesely)
+#            2016  Vincent Nguyen
+#            2016  Johns Hopkins University (Author: Daniel Povey)
+#            2018  François Hernandez
+#
+# Apache 2.0
+#
+# Project by Alexander Jermann (apj2125)
+#
+
+
+### Settings ###
+
+. ./path.sh || exit 1
+. ./cmd.sh || exit 1
+
+set -e -o pipefail -u
+
+nj=8
+decode_nj=8
+lm_order=3
+
+stage=0
+train_rnnlm=false
+train_lm=true
+
+. utils/parse_options.sh || exit 1
+[[ $# -ge 1 ]] && { echo "Wrong arguments!"; exit 1; }
+# delete MFCC
+rm -rf exp data/miami/train/cmvn.scp data/miami/train/feats.scp data/miami/train/split8 data/miami/test/cmvn.scp data/miami/test/feats.scp data/miami/test/split8 data/local/lang data/lang data/local/tmp data/local/dict/lexiconp.txt
+
+
+### Stages ###
+
+echo
+echo "===== PREPARING ACOUSTIC DATA ====="
+echo
+# Needs to be prepared by hand (or using self written scripts):
+#
+# spk2gender  [<speaker-id> <gender>]
+# wav.scp     [<uterranceID> <full_path_to_audio_file>]
+# text        [<uterranceID> <text_transcription>]
+# utt2spk     [<uterranceID> <speakerID>]
+# corpus.txt  [<text_transcription>]
+if [ $stage -le 0 ]; then
+  # ====== Download Miami =======
+  # local/download_miami_data.sh
+  # python3 local/process_miami_data.py
+  # python3 local/create_test_sets.py
+
+  # INSERT script to add absolute path to wav.scp
+  # local/download_miami_data_kaldi.sh
+  # local/fix_utt2spk.sh
+  # utils/utt2spk_to_spk2utt.pl data/miami/bangortalk/utt2spk > data/miami/bangortalk/spk2utt
+  # cat data/miami/bangortalk/text_cs data/miami/bangortalk/text_en data/miami/bangortalk/text_spa > data/miami/bangortalk/text
+  # sort -o data/miami/bangortalk/text data/miami/bangortalk/text
+  # local/make_train_test.sh
+
+  # Verify Data directory
+  utils/validate_data_dir.sh --no-feats data/miami/train || exit 1
+  utils/validate_data_dir.sh --no-feats data/miami/test || exit 1
+
+  # ==== Set-Up Commonvoice =====
+  #local/download_commonvoice.sh
+fi
+echo
+echo "===== FEATURES EXTRACTION ====="
+echo
+if [ $stage -le 1 ]; then
+  for set in test train; do
+    dir=data/miami/$set
+    steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" $dir
+    steps/compute_cmvn_stats.sh $dir
+  done
+fi
+
+echo
+echo "===== PREPARING LANGUAGE DATA ====="
+echo
+# Needs to be prepared by hand (or using self written scripts):
+#
+# lexicon.txt           [<word> <phone 1> <phone 2> ...]
+# nonsilence_phones.txt [<phone>]
+# silence_phones.txt    [<phone>]
+# optional_silence.txt  [<phone>]
+# Preparing language data
+if [ $stage -le 2 ]; then
+  # local/prepare_dict.sh
+  # Check that data dirs are okay!
+  utils/validate_dict_dir.pl data/local/dict || exit 1
+fi
+if [ $stage -le 3 ]; then
+  utils/prepare_lang.sh data/local/dict "<unk>" data/local/lang data/lang
+fi
+
+echo
+echo "===== TRAINING LM ====="
+echo "===== MAKING lm.arpa ====="
+echo
+if [ $stage -le 4 ]; then
+  loc=`which ngram-count`;
+  if [ -z $loc ]; then
+          if uname -a | grep 64 >/dev/null; then
+                  sdir=$KALDI_ROOT/tools/srilm/bin/i686-m64
+          else
+                          sdir=$KALDI_ROOT/tools/srilm/bin/i686
+          fi
+          if [ -f $sdir/ngram-count ]; then
+                          echo "Using SRILM language modelling tool from $sdir"
+                          export PATH=$PATH:$sdir
+          else
+                          echo "SRILM toolkit is probably not installed.
+                                  Instructions: tools/install_srilm.sh"
+                          exit 1
+          fi
+  fi
+  local=data/local
+  mkdir $local/tmp
+  ngram-count -order $lm_order -write-vocab $local/tmp/vocab-full.txt -wbdiscount -text $local/corpus.txt -lm $local/tmp/lm.arpa
+fi
+echo
+echo "===== MAKING G.fst ====="
+echo
+arpa2fst --disambig-symbol=#0 --read-symbol-table=data/lang/words.txt data/local/tmp/lm.arpa data/lang/G.fst
+echo
+echo "===== MONO TRAINING ====="
+echo
+steps/train_mono.sh --nj $nj --cmd "$train_cmd" data/miami/train data/lang exp/mono  || exit 1
+echo
+echo "===== MONO DECODING ====="
+echo
+utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/miami/test exp/mono/decode
+echo
+echo "===== MONO ALIGNMENT ====="
+echo
+steps/align_si.sh --nj $nj --cmd "$train_cmd" data/miami/train data/lang exp/mono exp/mono_ali || exit 1
+echo
+echo "===== TRI1 (first triphone pass) TRAINING ====="
+echo
+steps/train_deltas.sh --cmd "$train_cmd" 2000 11000 data/miami/train data/lang exp/mono_ali exp/tri1 || exit 1
+echo
+echo "===== TRI1 (first triphone pass) DECODING ====="
+echo
+utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph || exit 1
+steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/tri1/graph data/miami/test exp/tri1/decode
+echo
+echo "===== run.sh script is finished ====="
+echo
+echo "$0: success."
+exit 0
